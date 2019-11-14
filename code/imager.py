56f3eb342bb5f92fe7ab1093d752925257479aee
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-11-14 18:16:33 +0100

    properly cut out window of picture

--- code/imager.py
+++ code/imager.py
@@ -1,4 +1,4 @@
 def sliding_window(image, stepSize, windowSize):
     for y in range(0, image.size[1], stepSize):
         for x in range(0, image.size[0], stepSize):
-            yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+            yield (x, y, image.crop([x, y, x + windowSize[1], y + windowSize[0]]))
