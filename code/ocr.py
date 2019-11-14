cd7a217b69b22003df2320620103f0d359a643ac
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-11-14 18:27:50 +0100

    gate drawing behind logic check

--- code/ocr.py
+++ code/ocr.py
@@ -4,8 +4,11 @@ from imager import sliding_window
 
 img = Image.open("../dataset/detection-images/detection-1.jpg")
 for (x, y, window) in sliding_window(image=img, stepSize=8, windowSize=(20, 20)):
-    newImg = img.copy()
-    draw = ImageDraw.Draw(newImg) # Creates a copy of the image and draws on it
-    draw.rectangle((x,y) + (x + window.size[1], y + window.size[0]), fill=128)
-    print(f"X: {x}, Y: {y}, Window: {window.size}")
-    newImg.save(f"img{x}-{y}.png", "PNG")
+
+    # Conditionally draw square if the probability is considered high enough
+    if False:
+        newImg = img.copy()
+        draw = ImageDraw.Draw(newImg) # Creates a copy of the image and draws on it
+        draw.rectangle((x,y) + (x + window.size[1], y + window.size[0]), fill=128)
+        print(f"X: {x}, Y: {y}, Window: {window.size}")
+        newImg.save(f"img{x}-{y}.png", "PNG")
