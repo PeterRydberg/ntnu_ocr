d820a01758a6587990248069311e2738136ba862
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-11-14 15:16:03 +0100

    shape -> size

--- code/imager.py
+++ code/imager.py
@@ -1,4 +1,4 @@
 def sliding_window(image, stepSize, windowSize):
     for y in range(0, image.size[1], stepSize):
-        for x in range(0, image.shape[0], stepSize):
+        for x in range(0, image.size[0], stepSize):
             yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
