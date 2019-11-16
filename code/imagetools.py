5ef5efce83c0b3bf464472ee27d86ad55dee9e0b
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-11-16 13:16:48 +0100

    split red and grey square

--- code/imagetools.py
+++ code/imagetools.py
@@ -5,8 +5,13 @@ def sliding_window(image, stepSize, windowSize):
         for x in range(0, image.size[0], stepSize):
             yield (x, y, image.crop([x, y, x + windowSize[1], y + windowSize[0]]))
 
-def draw_red_square(x, y, target_image, window):
+def draw_red_square(x, y, target_image):
     draw = ImageDraw.Draw(target_image) 
-    draw.rectangle((x,y) + (x + window.size[1], y + window.size[0]), outline="#ff0000")
+    draw.rectangle((x,y) + (x + 20, y + 20), outline="#ff0000")
     # target_image.save("./dump/img1.png", "PNG")
+    return target_image
+
+def draw_grey_square(x, y, target_image, window):
+    draw = ImageDraw.Draw(target_image)
+    draw.rectangle((x,y) + (x + window.size[1], y + window.size[0]), outline="#333333")
     return target_image
